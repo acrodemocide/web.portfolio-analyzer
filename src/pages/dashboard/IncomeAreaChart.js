ece14496c3d9259654abb5e5105f4dcbc7f79470
Daniel Howard <acrodemocide@hotmail.com>
2022-11-06 09:40:18 -0700

    Removed comments we no longer need

--- src/pages/dashboard/IncomeAreaChart.js
+++ src/pages/dashboard/IncomeAreaChart.js
@@ -44,7 +44,6 @@ const IncomeAreaChart = ({ slot }) => {
             colors: [theme.palette.primary.main, theme.palette.primary[700]],
             xaxis: {
                 categories:
-                    // TODO: Update these slots to change how many positions you want to see in the graphs
                     slot === 'month'
                         ? ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
                         : ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
@@ -88,28 +87,17 @@ const IncomeAreaChart = ({ slot }) => {
         }));
     }, [primary, secondary, line, theme, slot]);
 
-    // TODO: acrodemocide -- figure out if we want more than one index to compare against
     const [series, setSeries] = useState([
         {
             name: 'Personal Portfolio',
-            // TODO: acrodemocide - Figure out why we can't have more than 7 data points in this case
-            data: [25, 50, 75, 100, 110, 130, 150] // , 155]
+            data: [25, 50, 75, 100, 110, 130, 150]
         },
         {
             name: 'S&P 500',
-            data: [25, 35, 55, 40, 65, 88, 98] // , 106]
+            data: [25, 35, 55, 40, 65, 88, 98]
         }
-        // {
-        //     name: 'Dow Jones',
-        //     data: [25, 20, 45, 35, 66, 69, 75] // , 64]
-        // },
-        // {
-        //     name: 'NASDAQ',
-        //     data: [25, 60, 50, 58, 72, 105, 88] // , 136]
-        // }
     ]);
 
-    // TODO: acrodemocide - Figure out how to have more than two colors for the charts.
     useEffect(() => {
         setSeries([
             {
@@ -120,14 +108,6 @@ const IncomeAreaChart = ({ slot }) => {
                 name: 'S&P 500',
                 data: slot === 'month' ? [25, 43, 55, 66, 60, 77, 68, 75, 88, 95, 100, 110] : [25, 35, 55, 40, 65, 88, 98] // , 106]
             }
-            // {
-            //     name: 'Dow Jones',
-            //     data: slot === 'month' ? [25, 20, 48, 67, 58, 88, 95, 105, 110, 120, 89, 72] : [25, 20, 45, 35, 66, 69, 75] // , 64]
-            // },
-            // {
-            //     name: 'NASDAQ',
-            //     data: slot === 'month' ? [25, 120, 105, 100, 98, 94, 86, 81, 76, 73, 63, 41] : [25, 60, 50, 58, 72, 105, 88] // , 136]
-            // }
         ]);
     }, [slot]);
 
