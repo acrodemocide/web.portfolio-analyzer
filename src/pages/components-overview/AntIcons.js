afe8e756b3e2113f3e8960d3a3ed333805232267
Daniel Howard <acrodemocide@hotmail.com>
2022-11-15 16:38:30 -0700

    Removed commented code

--- src/pages/components-overview/AntIcons.js
+++ src/pages/components-overview/AntIcons.js
@@ -2,7 +2,6 @@
 import { styled } from '@mui/material/styles';
 
 // project import
-// import { ComponentSkeleton } from './ComponentSkeleton';
 import ComponentSkeleton from './ComponentSkeleton';
 import MainCard from 'components/MainCard';
 
