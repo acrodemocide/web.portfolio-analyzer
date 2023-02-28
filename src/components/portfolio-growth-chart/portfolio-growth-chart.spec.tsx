853507085341b2c16b8f26450fb434a17df54752
Daniel Howard <acrodemocide@hotmail.com>
2023-02-27 17:52:18 -0700

    Created new React project with just the items we need and removed all the old template code that was not necessary

--- src/components/portfolio-growth-chart/portfolio-growth-chart.spec.tsx
+++
@@ -1,5 +0,0 @@
-describe('portfolio-growth-chart', () => {
-    it('passes the test', () => {
-        expect(true).toBe(true);
-    });
-});
---
+++ src/components/portfolio-growth-chart/portfolio-growth-chart.test.tsx
@@ -0,0 +1,8 @@
+import { PortfolioGrowthChart } from './portfolio-growth-chart';
+
+describe('portfolio-growth-chart', () => {
+    it('passes the test', () => {
+        expect(true).toBe(true);
+        expect(PortfolioGrowthChart).toBeDefined();
+    });
+});
