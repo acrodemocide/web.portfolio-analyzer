c70a146510f7374ba89e65cb4c00d6ccfc418fab
Daniel Howard <acrodemocide@hotmail.com>
2024-02-14 21:11:54 -0700

    Updated UI to get results from API

--- src/services/backtest-service.tsx
+++ src/services/backtest-service.tsx
@@ -1,16 +1,27 @@
 import axios from 'axios';
 
+// TODO: dhoward -- come up with a better return object to handle more data.
+//  For now, we're just going to return the price history.
 export type Portfolio = {
     price_history: number[];
 };
 
 export const BacktestPortfolio = (): Promise<Portfolio> => {
+    // TODO: dhoward -- this is a placeholder for the actual body
     const body = {
-        businessName: 'Tesla',
-        ticker: 'T',
-        price: '145.23'
+        "stocks": {
+            "asdf": 0.25,
+            "fdsa": 0.75
+        },
+        "strategy": "new_algorithm"
     };
     return axios.post('http://localhost:8000/stocks/backtest/', body).then((response) => {
-        return response.data;
+        console.log('@@response.data: ', response.data);
+        const snapshots = response.data.snapshots;
+        const price_history: number[] = [];
+        for (let key in snapshots) {
+            price_history.push(snapshots[key]);
+        }
+        return { price_history };
     });
 };
